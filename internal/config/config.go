ac11171823bdcc9280d72383fbaafa798c837ac2
Roman Sharkov <roman.scharkov@gmail.com>
2025-06-02 23:08:14 +0200

    fix: Remove obsolete CLI parameter

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,7 +9,6 @@ import (
 
 type ConfigGenerate struct {
 	Locale         language.Tag
-	Translations   []language.Tag
 	SrcPathPattern string
 	TrimPath       bool
 	JSON           bool
@@ -23,10 +22,8 @@ func ParseCLIArgsGenerate(osArgs []string) (*ConfigGenerate, error) {
 	c := &ConfigGenerate{}
 
 	var locale string
-	var translationLocales arrayFlags
 
 	cli := flag.NewFlagSet(osArgs[0], flag.ExitOnError)
-	cli.Var(&translationLocales, "t", "translation locales")
 	cli.StringVar(&locale, "l", "",
 		"default locale of the original source code texts in BCP 47")
 	cli.StringVar(&c.SrcPathPattern, "p", ".", "path to Go module")
@@ -41,16 +38,6 @@ func ParseCLIArgsGenerate(osArgs []string) (*ConfigGenerate, error) {
 		return nil, fmt.Errorf("parsing: %w", err)
 	}
 
-	for _, locale := range translationLocales {
-		l, err := language.Parse(locale)
-		if err != nil {
-			return nil, fmt.Errorf(
-				"argument 't' (%q) must be a valid BCP 47 locale: %w", locale, err,
-			)
-		}
-		c.Translations = append(c.Translations, l)
-	}
-
 	if locale != "" {
 		var err error
 		c.Locale, err = language.Parse(locale)
@@ -62,17 +49,4 @@ func ParseCLIArgsGenerate(osArgs []string) (*ConfigGenerate, error) {
 	}
 
 	return c, nil
-}
-
-type arrayFlags []string
-
-var _ flag.Value = new(arrayFlags)
-
-func (a *arrayFlags) String() string {
-	return fmt.Sprintf("%v", *a)
-}
-
-func (a *arrayFlags) Set(value string) error {
-	*a = append(*a, value)
-	return nil
 }
