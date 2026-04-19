e69526d761f218903aa95fc40ae73df3c4e8a21a
Roman Sharkov <roman.scharkov@gmail.com>
2026-04-20 00:39:36 +0200

    fix: Open hybrid app for toki edit

--- internal/cli/edit.go
+++ internal/cli/edit.go
@@ -4,12 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"net"
 	"os"
 	"path/filepath"
-	"time"
 
-	"github.com/cli/browser"
 	"github.com/romshark/toki/editor"
 	"github.com/romshark/toki/internal/log"
 )
@@ -20,7 +17,8 @@ type Edit struct{}
 func (e *Edit) Run(osArgs, env []string, stderr io.Writer) error {
 	fs := flag.NewFlagSet("edit", flag.ContinueOnError)
 	fs.SetOutput(stderr)
-	server := fs.String("server", "", "run as plain HTTP server on this address")
+	server := fs.String("server", "",
+		"run as plain HTTP server on this address instead of the native desktop app")
 	bundlePkg := fs.String("b", "tokibundle", "path to generated Go bundle package")
 	if err := fs.Parse(osArgs[2:]); err != nil {
 		return fmt.Errorf("%w: %w", ErrInvalidCLIArgs, err)
@@ -33,7 +31,7 @@ func (e *Edit) Run(osArgs, env []string, stderr io.Writer) error {
 		return fmt.Errorf("resolving working directory: %w", err)
 	}
 
-	_, s := editor.Setup(dir, *bundlePkg, Version, env,
+	a, s := editor.Setup(dir, *bundlePkg, Version, env,
 		CleanGenerated, GenerateBundle, ApplyChangesAndBuild)
 
 	if *server != "" {
@@ -41,26 +39,5 @@ func (e *Edit) Run(osArgs, env []string, stderr io.Writer) error {
 		return nil
 	}
 
-	// Default: start on free port and open browser.
-	ln, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		return fmt.Errorf("finding free port: %w", err)
-	}
-	addr := ln.Addr().String()
-	_ = ln.Close()
-
-	go func() {
-		// Wait for server to be ready, then open browser.
-		for {
-			conn, err := net.DialTimeout("tcp", addr, 100*time.Millisecond)
-			if err == nil {
-				_ = conn.Close()
-				break
-			}
-		}
-		_ = browser.OpenURL(fmt.Sprintf("http://%s", addr))
-	}()
-
-	os.Exit(editor.RunServer(s, addr))
-	return nil
+	return RunHybridApp(a, s)
 }
---
+++ internal/cli/edit_hybrid.go
@@ -0,0 +1,80 @@
+package cli
+
+// #cgo darwin LDFLAGS: -framework UniformTypeIdentifiers
+import "C"
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/wailsapp/wails/v3/pkg/application"
+
+	"github.com/romshark/toki/editor/app"
+	"github.com/romshark/toki/editor/datapagesgen"
+)
+
+// RunHybridApp launches the editor as a native Wails desktop app backed by
+// a local HTTP server on an ephemeral 127.0.0.1 port. Returns when the
+// webview window closes or the server fails to start.
+func RunHybridApp(a *app.App, s *datapagesgen.Server) error {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return fmt.Errorf("finding free port: %w", err)
+	}
+	addr := ln.Addr().String()
+	_ = ln.Close()
+
+	serverCtx, serverCancel := context.WithCancel(context.Background())
+	defer serverCancel()
+
+	wailsApp := application.New(application.Options{
+		Name: "Toki Editor",
+		Mac: application.MacOptions{
+			ApplicationShouldTerminateAfterLastWindowClosed: true,
+		},
+		OnShutdown: func() { serverCancel() },
+	})
+
+	a.PickDirectory = func() (string, error) {
+		return wailsApp.Dialog.OpenFile().
+			CanChooseDirectories(true).
+			CanChooseFiles(false).
+			SetTitle("Select Project Folder").
+			PromptForSingleSelection()
+	}
+
+	serverErr := make(chan error, 1)
+	go func() {
+		serverErr <- s.ListenAndServe(serverCtx, addr)
+	}()
+
+	// Wait until the server is accepting connections (or fails).
+	for {
+		select {
+		case err := <-serverErr:
+			return fmt.Errorf("server failed to start: %w", err)
+		default:
+		}
+		conn, dialErr := net.DialTimeout("tcp", addr, 100*time.Millisecond)
+		if dialErr == nil {
+			_ = conn.Close()
+			break
+		}
+	}
+
+	wailsApp.Window.NewWithOptions(application.WebviewWindowOptions{
+		Title:     "Toki Editor",
+		Width:     1200,
+		Height:    800,
+		MinWidth:  600,
+		MinHeight: 400,
+		URL:       fmt.Sprintf("http://%s", addr),
+	})
+
+	if err := wailsApp.Run(); err != nil {
+		return fmt.Errorf("wails: %w", err)
+	}
+	return nil
+}
